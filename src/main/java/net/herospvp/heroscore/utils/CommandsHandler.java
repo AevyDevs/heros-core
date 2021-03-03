0b997dff5e4e99406bc9d5ece750eeac305889ab
Nicolas <nicolasgiulixni.it@gmail.com>
2021-03-03 16:21:47 +0100

    1.1.20-SNAPSHOT

--- src/main/java/net/herospvp/heroscore/utils/CommandsHandler.java
+++ src/main/java/net/herospvp/heroscore/utils/CommandsHandler.java
@@ -9,7 +9,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public abstract class CommandsHandler implements CommandExecutor, TabCompleter {
@@ -18,25 +20,21 @@ public abstract class CommandsHandler implements CommandExecutor, TabCompleter {
     private final boolean tabComplete;
     private final List<String> usage;
     private String permission;
-    private final JavaPlugin instance;
+    private final JavaPlugin plugin;
 
-    public CommandsHandler(JavaPlugin instance,
-                           String permission,
-                           String command,
-                           boolean onlyPlayer,
-                           List<String> usage,
-                           boolean tabCompleteCustom
-    ) {
+    public CommandsHandler(JavaPlugin plugin, String permission, String command, boolean onlyPlayer, List<String> usage,
+                          boolean tabCompleteCustom) {
         this.onlyPlayer = onlyPlayer;
         this.usage = usage;
         if (permission != null) {
             this.permission = permission;
         }
         this.tabComplete = tabCompleteCustom;
-        this.instance = instance;
+        this.plugin = plugin;
 
-        instance.getCommand(command).setExecutor(this);
-        instance.getCommand(command).setTabCompleter(this);
+        plugin.getCommand(command).setExecutor(this);
+        plugin.getCommand(command).setTabCompleter(this);
+
     }
 
     public abstract boolean command(CommandSender sender, String[] args);
@@ -45,20 +43,19 @@ public abstract class CommandsHandler implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (onlyPlayer && sender instanceof ConsoleCommandSender) {
-            sender.sendMessage(ChatColor.RED + "Comando eseguibile solo dai player :(");
-            return false;
+        if (onlyPlayer) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Comando eseguibile solo dai player :(");
+                return false;
+            }
         }
 
         if (permission != null && !sender.hasPermission(permission)) {
-            sender.sendMessage(ChatColor.RED + "Permesso negato. ("  + permission + ")");
+            sender.sendMessage(ChatColor.RED + "Permesso negato.");
             return false;
         }
 
         if (!command(sender, args)) {
-            if (usage == null) {
-                return false;
-            }
             for (String s : usage) {
                 sender.sendMessage(StringUtils.c(s));
             }
@@ -68,16 +65,34 @@ public abstract class CommandsHandler implements CommandExecutor, TabCompleter {
         return true;
     }
 
+    public JavaPlugin getPlugin() {
+        return plugin;
+    }
+
     public List<String> getDefaultTabList(String[] args) {
         List<String> list = new ArrayList<>();
-        if (args[args.length - 1].equalsIgnoreCase("")) {
+        if (args[args.length-1].equalsIgnoreCase("")) {
             Bukkit.getOnlinePlayers().forEach((players) -> {
                 list.add(players.getName());
             });
         } else {
             for (Player player : Bukkit.getOnlinePlayers()) {
-                if (player.getName().startsWith(args[args.length - 1])) {
+                if (player.getName().startsWith(args[args.length-1])) {
                     list.add(player.getName());
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<String> getArgsTabList(String[] args) {
+        List<String> list = new ArrayList<>();
+        if (args[args.length-1].equalsIgnoreCase("")) {
+            return Arrays.asList(args);
+        } else {
+            for (String arg : args) {
+                if (arg.startsWith(args[args.length-1])) {
+                    list.add(arg);
                 }
             }
         }
@@ -86,7 +101,14 @@ public abstract class CommandsHandler implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        return tabComplete ? tabComplete(args) : getDefaultTabList(args);
+        if (tabComplete) {
+            if (args.length == 1) {
+                List<String> argList = tabComplete(args);
+                return argList.stream().filter(a -> a.startsWith(args[0])).collect(Collectors.toList());
+            }
+        }
+
+        return getDefaultTabList(args);
     }
 
 }
